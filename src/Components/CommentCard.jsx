45fe0554fbf9dc491ace3927fce7f5ba1a98ec03
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2025-09-03 11:40:32 +0100

    Comment Container
    Styling and skeleton added
    
    - Need to add logic and filter dropdowns

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,4 +1,11 @@
 import returnDateFormat from "../DataCalculations/dateConverted";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, far, fab)
 
 function CommentCard ({client}) {
 
@@ -9,11 +16,26 @@ function CommentCard ({client}) {
         <div className={positiveClass}>
             <p className="commentText">"{client.comments}"</p>
             <div className='commentInfo'>
-            <p className='commentTime'>{returnDateFormat(client.timestamp)}</p>
-            <p className='commentClinician' >{client.clinician}</p>
+                <p className='commentsRating'>Rating: <RatingStars /></p>
+                <p>{client.assessmentType}</p>
+                <p className='commentClinician' >{client.clinician}</p>
+                <p className='commentTime'>{returnDateFormat(client.timestamp)}</p>
             </div>
         </div>
     )
 }
 
 export default CommentCard;
+
+
+function RatingStars () {
+    return (
+        <>
+        <FontAwesomeIcon icon="fa-solid fa-star" className='starIcon'/>
+        <FontAwesomeIcon icon="fa-solid fa-star" className='starIcon'/>
+        <FontAwesomeIcon icon="fa-solid fa-star" className='starIcon'/>
+        <FontAwesomeIcon icon="fa-regular fa-star" className=''/>
+        <FontAwesomeIcon icon="fa-regular fa-star" className=''/>
+        </>
+    )
+}
--- src/Pages/Comments/Comments.jsx
+++ src/Pages/Comments/Comments.jsx
@@ -1,21 +1,27 @@
 import { useContext } from 'react';
 import CommentCard from '../../Components/CommentCard';
-import PageHeader from '../PageComponents/PageHeader';
 import resultsContext from '../../Context/resultsContext';
+import '../../CSS/CommentsPage.css'
 
 function Comments () {
 
     const { filteredResults } = useContext(resultsContext)
 
     return (
-        <>
-            <PageHeader title={'Comments'}/>
+        <div className='commentLayout'>
+            <div>
+                <button>Sort By</button>
+                <button>Satisfaction Rating</button>
+                <button>All Doctors</button>
+            </div>
+            <div className='commentContainer'>
             {
                 filteredResults.map((item) => {return (
                     <CommentCard key={item.id} client={item}/>
                 )})
             }
-        </>
+            </div>
+        </div>
     )
 }
 
