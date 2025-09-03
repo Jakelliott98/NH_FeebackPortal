cfb658f5bfe0e0cf89b1c7667c74f68a165aa4db
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2025-09-03 16:30:47 +0100

    Added the Sort By logic
    Added in the logic for sorting the comments

--- src/DataCalculations/helperFunctions.js
+++ src/DataCalculations/helperFunctions.js
@@ -23,4 +23,39 @@ function checkResponseType (results, responseFilter) {
   }
 }
 
-export { returnDrList, checkAssessmentType, checkResponseType }
+function returnOrderedList (sortBy, results) {
+
+  switch (sortBy) {
+
+    case 'Highest Rated': {
+      return results.toSorted((a ,b) => b.averageScore - a.averageScore)
+    }
+    case 'Lowest Rated': {
+      return results.toSorted((a,b) => a.averageScore - b.averageScore)
+    }
+    case 'Clinician (A-Z)': {
+      return results.toSorted((a, b) => {
+        if (a.clinician > b.clinician) {
+            return 1
+        } else if (a.clinician < b.clinician) {
+            return -1
+        } else {
+            return 0
+        }
+    })
+    }
+    case 'Most Recent': {
+      return results.sort((a, b) => {
+        return new Date(b.timestamp) - new Date(a.timestamp)
+    })
+    }
+    case 'Oldest First': {
+      return results.sort((a, b) => {
+        return new Date(a.timestamp) - new Date(b.timestamp)
+    })
+    }
+    default: return results;
+  }
+}
+
+export { returnOrderedList, returnDrList, checkAssessmentType, checkResponseType }
--- src/Pages/Comments/Comments.jsx
+++ src/Pages/Comments/Comments.jsx
@@ -1,12 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useState, useMemo } from 'react';
 import CommentCard from '../../Components/CommentCard';
 import resultsContext from '../../Context/resultsContext';
 import '../../CSS/CommentsPage.css'
 import { GenericFilterDropdown } from '../../Components/DropdownFilter';
-import { returnDrList } from '../../DataCalculations/helperFunctions';
-import { useMemo } from 'react';
+import { returnDrList, returnOrderedList } from '../../DataCalculations/helperFunctions';
 
-let sortByOptions = ['Rating', 'Doctor', 'Date', 'Satisfaction']
+let sortByOptions = ['Clinician (A-Z)', 'Highest Rated','Lowest Rated', 'Most Recent', 'Oldest First']
 
 // Hold a state of the dr name etc.
 // Add a function for filtereing the results by the dr.
@@ -15,17 +14,19 @@ function Comments () {
 
     const { filteredResults } = useContext(resultsContext)
     const turnToState = 'All Doctors';
-    const sortBy = 'Sort By';
     const ratingsTitle = 'Satisfaction Rating';
 
     let DoctorList =  useMemo(() => {
         return returnDrList(filteredResults)
     }, [filteredResults])
 
+    const [sortBy, setSortBy] = useState('Sort By');
+    let organisedList = returnOrderedList(sortBy, filteredResults)
+    
     return (
         <div className='commentLayout'>
             <div className='CommentFilters'>
-                <GenericFilterDropdown resultFilter={sortBy} arrayData={sortByOptions} />
+                <GenericFilterDropdown resultFilter={sortBy} arrayData={sortByOptions} filterbyFunction={setSortBy}/>
                 <div className='commentDropdowns'>
                 <GenericFilterDropdown resultFilter={ratingsTitle} arrayData={DoctorList}/>
                 <GenericFilterDropdown resultFilter={turnToState} arrayData={DoctorList} />
@@ -33,7 +34,7 @@ function Comments () {
             </div>
             <div className='commentContainer'>
             {
-                filteredResults.map((item) => {return (
+                organisedList.map((item) => {return (
                     <CommentCard key={item.id} client={item}/>
                 )})
             }
