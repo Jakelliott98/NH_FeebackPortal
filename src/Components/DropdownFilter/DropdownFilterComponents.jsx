04f420c1903757769d02fedd8b5088e3ad6046da
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2025-09-12 14:52:44 +0100

    Cleaned up the rating dropdown container
    Need to add a function to hover the stars. Stars icon turn from solid to regular obMouseOver

--- src/Components/DropdownFilter/DropdownFilterComponents.jsx
+++ src/Components/DropdownFilter/DropdownFilterComponents.jsx
@@ -41,12 +41,12 @@ function RatingFilterCard ({onSelect, currentRating}) {
         }
 
     return (
-        <div className='dropdownOptionComponent'>
-            <FontAwesomeIcon icon={isStared[0] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[0] ? 'starIcon' : ''} onClick={() => {onSelect(1)}}/>
-            <FontAwesomeIcon icon={isStared[1] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[1] ? 'starIcon' : ''} onClick={() => {onSelect(2)}}/>
-            <FontAwesomeIcon icon={isStared[2] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[2] ? 'starIcon' : ''} onClick={() => {onSelect(3)}}/>
-            <FontAwesomeIcon icon={isStared[3] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[3] ? 'starIcon' : ''} onClick={() => {onSelect(4)}}/>
-            <FontAwesomeIcon icon={isStared[4] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[4] ? 'starIcon' : ''} onClick={() => {onSelect(5)}}/>
+        <div className='dropdownOptionComponent ratingContainer'>
+            <FontAwesomeIcon icon={isStared[0] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[0] ? 'starIcon ratingIcon' : 'ratingIcon'} onClick={() => {onSelect(1)}}/>
+            <FontAwesomeIcon icon={isStared[1] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[1] ? 'starIcon ratingIcon' : 'ratingIcon'} onClick={() => {onSelect(2)}}/>
+            <FontAwesomeIcon icon={isStared[2] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[2] ? 'starIcon ratingIcon' : 'ratingIcon'} onClick={() => {onSelect(3)}}/>
+            <FontAwesomeIcon icon={isStared[3] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[3] ? 'starIcon ratingIcon' : 'ratingIcon'} onClick={() => {onSelect(4)}}/>
+            <FontAwesomeIcon icon={isStared[4] ? 'fa-star fa-solid' : 'fa-regular fa-star'} className={isStared[4] ? 'starIcon ratingIcon' : 'ratingIcon'} onClick={() => {onSelect(5)}}/>
         </div>
     )
 }
