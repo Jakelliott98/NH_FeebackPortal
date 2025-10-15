a100fc9c682f6ad65eccf267244b5308081a8f41
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2025-10-15 11:40:49 +0100

    Added env file and removed key & url

--- src/Utils/Data/fetchAPIData.js
+++ src/Utils/Data/fetchAPIData.js
@@ -1,7 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://eenszeklhitnbwhahbco.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlbnN6ZWtsaGl0bmJ3aGFoYmNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2OTA4MjAsImV4cCI6MjA3NDI2NjgyMH0.rhzXKa5qfF23-wSFRe0nP7i-Lhwrwy5D2lLQ25iz5r8'
-const supabase = createClient(supabaseUrl, supabaseKey)
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
 
 export default supabase;
